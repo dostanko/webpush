abefcd3e35aad9cffed753a9f1ca54e228144d97
Iryna Dastanka <Iryna_Dastanka@epam.com>
2013-08-09 13:20:15 +0300

    hello websocket js+ruby

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source "https://rubygems.org"
 gem 'sinatra', '1.1.0'
+gem 'em-websocket', '~> 0.5.0'
--- web.rb
+++ web.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
+require 'em-websocket'
+
+@websocket_alive = false
 
 get '/' do
-  "Hello, world"
+  Thread.new { start_websoket} if !@websocket_alive
+  erb :dash
+end
+
+def start_websoket
+  EM.run {
+    EM::WebSocket.run(:host => "0.0.0.0", :port => 9876) do |ws|
+      ws.onopen { |handshake|
+        puts "WebSocket connection open"
+
+        # Access properties on the EM::WebSocket::Handshake object, e.g.
+        # path, query_string, origin, headers
+
+        ws.send "Hello Client, you connected !!! to #{handshake.path}"
+      }
+
+      ws.onclose { puts "Connection closed" }
+
+      ws.onmessage { |msg|
+        puts "Recieved message: #{msg}"
+        ws.send "Pong: #{msg}"
+      }
+    end
+  }
 end
